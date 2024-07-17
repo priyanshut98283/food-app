291e6d1f18638a3c4ae2e0571b11e94b4149fafb
Priyanshu Tiwari <priyanshut98283@gmail.com>
2024-07-17 16:35:10 +0530

    ui-code

---
+++ src/consts/categories.js
@@ -0,0 +1,8 @@
+const categories = [
+  {id: '1', name: 'pizza', image: require('../../src/assets/catergories/pizza.png')},
+  {id: '2', name: 'Burger', image: require('../../src/assets/catergories/burger.png')},
+  {id: '3', name: 'Veg Meal', image: require('../../src/assets/catergories/meal-veg.jpeg')},
+  {id: '4', name: 'Salad', image: require('../../src/assets/catergories/salad.png')},
+];
+
+export default categories;
---
+++ src/consts/foods.js
@@ -0,0 +1,32 @@
+const foods = [
+  {
+    id: '1',
+    name: 'Normal thali',
+    ingredients: 'Chapati,rice,dal,sabji',
+    price: '8.30',
+    image: require('../assets/food-image-thali-normal.jpeg'),
+  },
+  {
+    id: '2',
+    name: 'Samosa',
+    ingredients: '4 samosas',
+    price: '7.10',
+    image: require('../assets/food-image-samosa.jpeg'),
+  },
+  {
+    id: '3',
+    name: 'Special thali',
+    ingredients: 'Chapati,rice,dal,sabji,sweets',
+    price: '5.10',
+    image: require('../assets/food-image-thali-special.jpeg'),
+  },
+  {
+    id: '4',
+    name: 'Paratha',
+    ingredients: '2 pieces paratha',
+    price: '9.55',
+    image: require('../assets/food-image-paratha.jpeg'),
+  },
+];
+
+export default foods;
